dcd94dff7f8d2c1185ad3e48563126d1ff53dc67
vikasgarg2105 <vikasgarg2105@gmail.com>
2022-07-11 09:14:12 +0530

    service page

--- src/components/pageTitle/PageTitle.jsx
+++ src/components/pageTitle/PageTitle.jsx
@@ -1,23 +1,27 @@
-import React from 'react'
+import React from "react";
 import "../pageTitle/PageTitle.scss";
 import { Container, Row, Col } from "react-bootstrap";
+import CircleAnim from "../../images/circle-anim.svg"
 
 const PageTitle = (props) => {
   return (
     <>
-        <div className="page-title position-relative">
-            <Container>
-                <Row>
-                    <Col>
-                        <div className="page-title-heading text-center text-white d-flex justify-content-center align-items-center">
-                            <h1>{props.Pagename}</h1>
-                        </div>
-                    </Col>
-                </Row>
-            </Container>
-        </div>
+      <div className="page-title position-relative">
+        <Container>
+          <Row>
+            <Col>
+              <div className="page-title-heading text-center text-white d-flex justify-content-center align-items-center">
+                <h1>{props.Pagename}</h1>
+                <div className="circle-animation">
+                    <img src={CircleAnim} alt="" className="img-fluid" />
+                </div>
+              </div>
+            </Col>
+          </Row>
+        </Container>
+      </div>
     </>
-  )
-}
+  );
+};
 
-export default PageTitle
+export default PageTitle;
